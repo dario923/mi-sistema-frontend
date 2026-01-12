7a64067b3781dd776f838c2924e0e862a015d7f7
dario sanabria <130596720+dario923@users.noreply.github.com>
2026-01-12 15:32:49 -0300

    Update main.ts
    cambio n

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-//import { environment } from '@env/environment';
+//import { environment } from './environments/environment';
+import { environment } from '@env/environment';
 
 if (environment.production) {
   enableProdMode();
@@ -11,6 +11,7 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
 
 
 
